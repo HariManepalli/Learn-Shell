d5b6f70ffc298005b0781fa0b6575ee7397aae33
Hariprasad <hari20185@gmail.com>
2023-02-19 19:52:43 +0530

    Print

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,18 @@
 echo Hello World
+
+## Color Syntax
+# echo -e "\e[COLm MESSAGE \e[0m"
+# echo -e -> Enable colors from echo command
+# \e[COLm -> Enable to particular color
+# Color     codes
+# Red     -> 31
+# Green   -> 32
+# Yellow  -> 33
+# Bue     -> 34
+# Magenta -> 35
+# Cyan    -> 36
+
+# \e[0m  -> Zero code is going to RESET the color, if we enable to color for sure we need to disable it,otherwise color will continue on screen.
+
+echo -e "\e[31mHello in red color \e[0m"
+echo Color is in white
