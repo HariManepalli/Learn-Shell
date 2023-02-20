ccd918ef055bd70e8df736432fe77f7db2734818
Hariprasad <hari20185@gmail.com>
2023-02-20 10:41:49 +0530

    Variable L1-point 2

---
+++ Input.sh
@@ -0,0 +1,12 @@
+# 0-n, $*/$@ , $#
+
+echo "First argument $1" # First argument
+echo "Second argument $2" # Second argument
+
+echo "Script name $0" # Script name
+
+echo All Arguments = $* # All Arguments
+echo All Arguments = $@ # All Arguments
+
+echo "Number of Arguments $#" # Number of Arguments
+
