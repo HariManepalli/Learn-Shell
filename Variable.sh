aaa2174fa04073ab28cd7a8e805726304eb982cf
Hariprasad <hari20185@gmail.com>
2023-02-20 10:25:34 +0530

    Variable L1-point 2

--- Variable.sh
+++ Variable.sh
@@ -1,8 +1,10 @@
 Date=2023-02-20
-
 echo "Hello, today's date is ${Date}"
 
 # Command substitution
 DATE=$(date)
-
 echo " Hello, today's date is ${DATE}"
+
+#Arthimetic substitution
+Add=$((34+89))
+echo "Addition of add = ${Add}"
