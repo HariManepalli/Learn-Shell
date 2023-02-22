96c8f571afe6158422c55b616b7a224e0688feb4
Hariprasad <hari20185@gmail.com>
2023-02-22 18:03:20 +0530

    Conditions 1

--- conditions.sh
+++ conditions.sh
@@ -1,13 +1,11 @@
 number=$1
 String=$2
 
-if [ "${number}" -eq 5 ];
-then
+if [ "${number}" -eq 5 ]; then
   echo number is 5
 fi
 
-if [ "${string}" == abc ];
-then
+if [ "${string}" == abc ]; then
   echo string is abc
 else
   echo string is not abc
